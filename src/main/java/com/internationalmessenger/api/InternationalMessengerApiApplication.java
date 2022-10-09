c50674846f5e1578a6c97cf7594f36c03e4a455c
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2022-10-09 22:14:05 +0500

    init commit

---
+++ src/main/java/com/internationalmessenger/api/InternationalMessengerApiApplication.java
@@ -0,0 +1,13 @@
+package com.internationalmessenger.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalMessengerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternationalMessengerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/internationalmessenger/api/InternationalMessengerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internationalmessenger.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalMessengerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
